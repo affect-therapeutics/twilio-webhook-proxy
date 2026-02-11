dd198fc1b911cbfb8703ac20725676cd1b29b16a
Russell Osborne <russell@joinaffect.com>
2026-02-10 19:00:31 -0500

    Switches to nix flake
    [AP-7911]

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Twilio Webhook proxy development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs_22
+          ];
+
+          shellHook = ''
+            echo "Node.js $(node --version)"
+          '';
+        };
+      }
+    );
+}
